9a0decb25fe538faf0e813e780c7941853404a46
Samuel Hand <handuel13@gmail.com>
2013-03-28 09:36:46 +0000

    Basic window creation, based off mad marx tutorial 1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//inlude our skeleton file
+#include "OgreSkeleton.h"
+
+//main method
+
+int main(int argc, char** argv) {
+	try {
+		OgreSkeleton* skeleton = new OgreSkeleton("Difference Engine");
+		//while the window isn't closed
+		while (!skeleton->window->isClosed()) {
+			//update the window
+            skeleton->window->update(false);
+            //swap the buffers using vsync
+            skeleton->window->swapBuffers(true);
+            skeleton->root->renderOneFrame();
+            //get events
+            WindowEventUtilities::messagePump();
+		}
+	//several things could go wrong, if they do, catch them here	
+	} catch (Exception &e) {
+        LogManager::getSingleton().logMessage(String("Ogre Error: ") + e.what());
+    } catch (std::exception &e) {
+        LogManager::getSingleton().logMessage(String("Error: ") + e.what());
+    }
+}
