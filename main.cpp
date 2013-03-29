c5452d58956fe364ed1957c333bc469ea72d3c7f
Samuel Hand <handuel13@gmail.com>
2013-03-29 10:50:48 +0000

    Mesh loading. Based of MadMarx tutorial 5.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-//Mesh creation based of mad marx tutorial 4
-//inlude our skeleton file
+//Mesh loading based of mad marx tutorial 5
+//include our skeleton file
 #include "OgreSkeleton.h"
 
 //main method
@@ -42,109 +42,46 @@ int main() {
         skeleton->window->setAutoUpdated(false);
         //clear events so we don't have to process everything we missed while initialising
         skeleton->root->clearEventTimes();
-        //our object object :D
-        ManualObject* manualObject = NULL;
-        {
-            //create the object
-            manualObject = scene->createManualObject("CubeWithAxes");
-            //don't update 2d vertex/index later
-            manualObject->setDynamic(false);
-            //size
-            float size = 0.7f;
-            //create the cube
-            //begin declaring vertices and faces for the shape, first parameter is the material
-            manualObject->begin("BaseWhiteNoLighting", RenderOperation::OT_TRIANGLE_LIST);
-            {
-                float cp = 1.0f * size;
-                float cm = -1.0f * size;
-                //vertices
-                manualObject->position(cm, cp, cm); //a vertex
-                manualObject->colour(ColourValue(0.0f, 1.0f, 0.0f, 1.0f));
-                manualObject->position(cp, cp, cm); //a vertex
-                manualObject->colour(ColourValue(1.0f, 1.0f, 0.0f, 1.0f));
-                manualObject->position(cp, cm, cm); //a vertex
-                manualObject->colour(ColourValue(0.0f, 1.0f, 0.0f, 1.0f));
-                manualObject->position(cm, cm, cm); //a vertex
-                manualObject->colour(ColourValue(1.0f, 0.0f, 0.0f, 1.0f));
-                manualObject->position(cm, cp, cp); //a vertex
-                manualObject->colour(ColourValue(0.0f, 0.0f, 0.0f, 1.0f));
-                manualObject->position(cp, cp, cp); //a vertex
-                manualObject->colour(ColourValue(0.0f, 1.0f, 1.0f, 1.0f));
-                manualObject->position(cp, cm, cp); //a vertex
-                manualObject->colour(ColourValue(1.0f, 1.0f, 1.0f, 1.0f));
-                manualObject->position(cm, cm, cp); //a vertex
-                manualObject->colour(ColourValue(0.0f, 0.0f, 1.0f, 1.0f));
-                //faces
-                manualObject->triangle(0, 1, 2);
-                manualObject->triangle(2, 3, 0);
-                manualObject->triangle(4, 6, 5);
-                manualObject->triangle(6, 4, 7);
-
-                manualObject->triangle(0, 4, 5);
-                manualObject->triangle(5, 1, 0);
-                manualObject->triangle(2, 6, 7);
-                manualObject->triangle(7, 3, 2);
-
-                manualObject->triangle(0, 7, 4);
-                manualObject->triangle(7, 0, 3);
-                manualObject->triangle(1, 5, 6);
-                manualObject->triangle(6, 2, 1);
-            }
-            manualObject->end();
-            //create the axis
-            manualObject->begin("BaseWhiteNoLighting", RenderOperation::OT_LINE_LIST);
-            {
-                float axeSize = 2.0f * size;
-                manualObject->position(0.0f, 0.0f, 0.0f);
-                manualObject->colour(Ogre::ColourValue::Red);
-                manualObject->position(axeSize, 0.0f, 0.0f);
-                manualObject->colour(Ogre::ColourValue::Red);
-                manualObject->position(0.0f, 0.0f, 0.0f);
-                manualObject->colour(Ogre::ColourValue::Green);
-                manualObject->position(0.0, axeSize, 0.0);
-                manualObject->colour(Ogre::ColourValue::Green);
-                manualObject->position(0.0f, 0.0f, 0.0f);
-                manualObject->colour(Ogre::ColourValue::Blue);
-                manualObject->position(0.0, 0.0, axeSize);
-                manualObject->colour(Ogre::ColourValue::Blue);
-
-                manualObject->index(0);
-                manualObject->index(1);
-                manualObject->index(2);
-                manualObject->index(3);
-                manualObject->index(4);
-                manualObject->index(5);
-            }
-            manualObject->end();
-        }
-        //create a mesh from the object
-        String meshName = "MeshCubeAndAxe";
-        manualObject->convertToMesh(meshName);
-        //create 5 entities from the mesh
-        const int entityNumber = 5;
-        SceneNode * nodes[entityNumber];
-        for (int iter = 0; iter < entityNumber; ++iter) {
-            Entity* entity = scene->createEntity(meshName);
+        //Get the singleton for the resource group manager class
+        ResourceGroupManager& rgMgr = ResourceGroupManager::getSingleton();
+        //create a resource group called meshes
+        String resourceGroupName = "meshes";
+        rgMgr.createResourceGroup(resourceGroupName);
+        //tell the resource group which directory to look in
+        rgMgr.addResourceLocation("./media/meshes", "FileSystem", resourceGroupName, false);
+        //parse any scripts in the resource group
+        rgMgr.initialiseResourceGroup(resourceGroupName);
+        //load all the files
+        rgMgr.loadResourceGroup(resourceGroupName);
+        //amount of entities to display
+        int entityNumber = 5;
+        SceneNode* nodes[entityNumber];
+        for (int iter = 0; iter < entityNumber; iter++) {
+            //the entity loaded from the Cube.mesh file
+            Entity* entity = scene->createEntity("Cube.mesh");
+            //create a scene node for the entity
             nodes[iter] = rootSceneNode->createChildSceneNode();
+            //attach the entity
             nodes[iter]->attachObject(entity);
-            //move the entity away from the others
-            float positionOffset = float(1 + iter * 2)-(float(entityNumber));
-            nodes[iter]->translate(positionOffset, positionOffset, -10.0f);
+            //scale up node
+            nodes[iter]->scale(Vector3(15,15,15));
+            //move nodes so they don't overlap
+            float offset = (float(1+ iter * 2) - (float(entityNumber)))*20;
+            nodes[iter]->translate(offset, offset, -200.0f);
         }
-        //while the window isn't closed
+        //while the window hasn't been closed
         while (!skeleton->window->isClosed()) {
-            //update the window
+            //update the window but don't swap buffers
             skeleton->window->update(false);
             //swap the buffers using vsync
             skeleton->window->swapBuffers(true);
             skeleton->root->renderOneFrame();
-            //rotate entities
-            for(int iter = 0; iter < entityNumber; iter++){
-                nodes[iter]->yaw(Radian(0.01f));
-                nodes[iter]->roll(Radian(0.01f));
-            }
             //get events
             WindowEventUtilities::messagePump();
+            for(int iter = 0; iter < entityNumber; iter++){
+                nodes[iter]->yaw(Radian(0.01f));
+                nodes[iter]->pitch(Radian(0.01f));
+            }
         }
         //several things could go wrong, if they do, catch them here	
     } catch (Exception &e) {
